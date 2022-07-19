b3f318bdbf0c685589d5aa34d8ea3e5153791485
Johann Guepjop <jguepjop@jguepjop-mn1.linkedin.biz>
2022-07-19 15:30:32 -0700

    completed remix-postgres connection andadded new benchmarck files

--- remix-app/remix-app/app/routes/small-json-payload/index.tsx
+++ remix-app/remix-app/app/routes/small-json-payload/index.tsx
@@ -4,8 +4,32 @@ import { loadRouteModule } from "@remix-run/react/dist/routeModules";
 import { Link } from "react-router-dom";
 import { get_small_payload } from "~/models/small-payload";
 
-export async function loader() {
+export const loader = async () => {
+  const { Client } = require("pg");
+  const dotenv = require("dotenv");
+  dotenv.config();
+
+  const connectDb = async () => {
+    try {
+      const client = new Client({
+        user: process.env.PGUSER,
+        host: process.env.PGHOST,
+        database: process.env.PGDATABASE,
+        password: process.env.PGPASSWORD,
+        port: process.env.PGPORT,
+      });
+
+      await client.connect();
+      const res = await client.query(
+        "SELECT data FROM payload Where title = 'small'"
+      );
+      await client.end();
+      return res;
+    } catch (error) {
+      return error;
+    }
+  };
   return json({
-    small_payload: get_small_payload(),
+    data: await connectDb(),
   });
-}
+};
