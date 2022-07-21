daf9b5e8d53bd86fc76acbcd9fbe159151be0180
Johann Guepjop <jguepjop@jguepjop-mn1.linkedin.biz>
2022-07-21 11:38:59 -0700

    tracking changes to remix app

--- remix-app/remix-app/app/routes/small-json-payload/index.tsx
+++ remix-app/remix-app/app/routes/small-json-payload/index.tsx
@@ -3,6 +3,8 @@ import { getDB } from "~/models/database.server";
 
 export const loader = async () => {
   const client = getDB();
+  client.connect();
+
   const res = await client.query(
     "SELECT data FROM payload Where title = 'small'"
   );
